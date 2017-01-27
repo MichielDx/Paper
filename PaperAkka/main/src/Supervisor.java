319ba343967f2be9faed48eb50ab5384e98767e4
MichielDx <michieldx@gmail.com>
2017-01-27 14:48:56 +0100

    akkaaaaaaaaaaaaaa

--- PaperAkka/main/src/Supervisor.java
+++ PaperAkka/main/src/Supervisor.java
@@ -1,11 +1,22 @@
 import akka.actor.*;
 import akka.japi.pf.DeciderBuilder;
-import akka.japi.pf.ReceiveBuilder;
 import scala.concurrent.duration.Duration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static akka.actor.SupervisorStrategy.*;
 
-public class Supervisor extends AbstractLoggingActor {
+public class Supervisor extends UntypedActor {
+    private List<ActorRef> children;
+
+    public Supervisor() {
+        this.children = new ArrayList<>();
+        final ActorRef child2 = getContext().actorOf(Builder.props(), "child2");
+        final ActorRef child1 = getContext().actorOf(Worker.props(), "child");
+        this.children.add(child1);
+        this.children.add(child2);
+    }
 
     public static final OneForOneStrategy STRATEGY = new OneForOneStrategy(
             10,
@@ -16,7 +27,7 @@ public class Supervisor extends AbstractLoggingActor {
     );
 
 
-    {
+    /*{
 
         final ActorRef child2 = getContext().actorOf(Builder.props(), "child2");
 
@@ -26,6 +37,11 @@ public class Supervisor extends AbstractLoggingActor {
                 .matchAny(any -> getContext().getChildren().forEach(child-> child.forward(any, getContext())))
                 .build()
         );
+    }*/
+
+    @Override
+    public void onReceive(Object message) throws Throwable {
+        children.forEach(child -> child.tell(message,getSelf()));
     }
 
     @Override
